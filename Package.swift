412894c940faa45b919d09ddd4161ac99ba982ae
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2022-12-04 22:31:28 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         // Swift NIO for all things networking
-        .package(url: "https://github.com/timechain-academy/swift-nio.git", .branch("b0effbcfd9c92246df01e2aed3f7e3c23f14aae4"),
+        .package(url: "https://github.com/timechain-academy/swift-nio.git", .branch("b0effbcfd9c92246df01e2aed3f7e3c23f14aae4")),
         .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.0.0"),
         // 💻 APIs for creating interactive CLI tools.
         .package(url: "https://github.com/vapor/console-kit.git", from: "4.0.0"),
